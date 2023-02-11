00fd4dea380941b7d64fc40283071dedde5db556
DiegoHdz7 <diego.hdz.glez@gmail.com>
2023-02-11 12:46:25 -0500

    first final version

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Login from './Login'
+import { useState, useRef, useEffect} from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  redirect,
+  useNavigate
+} from "react-router-dom";
+
+import Comments from './Comments';
+import Thankyou from './ThankYou';
+import { Button } from 'bootstrap';
+
+
+
 
 function App() {
+
+   //States
+   const [email, setEmail] = useState('');
+   const [isLogin, setIsLogin] = useState(false);
+   const [comment, setComment] = useState('');
+   const [users, setUsers]=useState([]);
+
+  
+   
+   //Json props
+
+   const appProps = {
+    email:email,
+    setEmail:setEmail,
+    isLogin:isLogin,
+    setIsLogin:setIsLogin,
+    comment:comment,
+    setComment:setComment,
+    users:users,
+    setUsers:setUsers
+   }
+
+   //useEffect
+   useEffect(() => {
+    document.title = 'Diego Hernandez Lab 1';
+  }, []);
+
+  
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   
+    
+    <Router>
+      
+ 
+ 
+      <Routes>
+        <Route path ='/' element= {<Login appProps={appProps}/>}/>
+        <Route path ='/thankyou' element= {<Thankyou email={email} comment={comment} />}/>
+        <Route path ='/test' element= {<Login appProps={appProps}/>}/>
+
+      </Routes>
+      
+
+    </Router>
+    </>
+    
   );
 }
+
+
+
+
 
 export default App;
